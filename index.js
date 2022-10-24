65cfdd9097af14eb28938d0ee1b48280b1bc0d94
aroy15 <anjonroy57@gmail.com>
2022-10-25 03:31:43 +0600

    courses API created

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/',(req, res)=>{
     res.send('Education API is running')
 });
+
+app.get('/courses', (req, res)=>{
+    res.send(courses);
+})
 
 app.listen(port,()=>{
     console.log(`Education server running on port: ${port} `)
