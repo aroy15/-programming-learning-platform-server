1f517ddbe98eaf25519cc9aa986436e42b3fa0dd
aroy15 <anjonroy57@gmail.com>
2022-10-25 04:23:46 +0600

    courses dynamic id API created

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/courses', (req, res)=>{
     res.send(courses);
 })
 
+app.get('/courses/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectCourse = courses.find(course => course._id === id);
+    res.send(selectCourse);
+})
+
 app.listen(port,()=>{
     console.log(`Education server running on port: ${port} `)
 })
