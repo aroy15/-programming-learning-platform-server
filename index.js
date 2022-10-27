aafbdca67a8f74a6842030a0bdb9b3d68a1581e0
aroy15 <anjonroy57@gmail.com>
2022-10-27 20:00:19 +0600

    blog data added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const blog = require('./data/blog.json')
 
 app.get('/',(req, res)=>{
     res.send('Education API is running')
@@ -19,6 +20,10 @@ app.get('/courses/:id', (req, res)=>{
     const id = req.params.id;
     const selectCourse = courses.find(course => course._id === id);
     res.send(selectCourse);
+})
+
+app.get('/blog', (req, res)=>{
+    res.send(blog)
 })
 
 app.listen(port,()=>{
